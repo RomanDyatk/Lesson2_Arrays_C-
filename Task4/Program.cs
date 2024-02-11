09d3632dbaff817eaf2e0488bad9ea7ff7807a91
Roman <dmot.boss@mail.ru>
2024-02-11 19:55:46 +0300

    add Lesson2 Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = 5;
+int[] arr = { 1, 2, 3, 4, 5 };
+int max = arr[0];
+for (int i = 0; i < n; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+
+Console.WriteLine(max);
+
+max = arr[0];
+foreach (int a in arr)
+{
+    if (a > max)
+    {
+        max = a;
+    }
+}
+
+Console.WriteLine(max);
