da344f0165d6d4b79cb91fbf35d31fdefb5fbfdc
Roman <dmot.boss@mail.ru>
2024-02-11 19:27:33 +0300

    add Lesson2 Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] arr = new int[N];
+
+// for (int i = 0; i < N; i++)
+// {
+//     arr[i] = i + 1;
+// }
+
+// // Вывод массива
+// for (int i = 0; i < N; i++)
+// {
+//     Console.Write(arr[i] + " ");
+// }
+
+// Задача №2
+
+
