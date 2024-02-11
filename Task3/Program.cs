d36eaacbd935cdabaa75ba0268e3f0b600a8214a
Roman <dmot.boss@mail.ru>
2024-02-11 19:33:26 +0300

    add Lesson2 Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr1 = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    arr1[i] = i + 1;
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (arr1[i] % 2 == 0)
+    {
+        Console.Write($"{arr1[i]} ");
+    }
+}
